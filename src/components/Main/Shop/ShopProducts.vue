d01daf3a094644d0da1a48e776b7e0c5f7ab6114
Gilbert <gssenyonjo@gmail.com>
2022-09-09 22:06:40 +0300

    made some changes to get app to run

--- src/components/Main/Shop/ShopProducts.vue
+++ src/components/Main/Shop/ShopProducts.vue
@@ -1,67 +1,73 @@
 <template>
     <div id="shop__products">
         <div v-for="product in products">
-			<shop-product :product="product"></shop-product>
+            <shop-product :product="product"></shop-product>
         </div>
     </div>
 </template>
 
 <script>
-	import ShopProduct from './ShopProduct'
-
-	
+import ShopProduct from './ShopProduct'
 
 
-    export default {
-        name: "ShopProducts",
-		components: {
-			'ShopProduct': ShopProduct
-		},
 
-		data: function() {
-			return {
 
-			}
-		}, 
-		methods: {
+export default {
+    name: "ShopProducts",
+    components: {
+        'ShopProduct': ShopProduct
+    },
 
-		},
-		computed: {
-			products: function() {
-				return this.$store.getters.getProducts
-			}
-		}
+    data: function () {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+        products: function () {
+            let myproducts = [
+                {
+                    name: "Samsung S7 Edged",
+                    price: 450000,
+                    src: "/dist/samsungs7.jpeg"
+                }, {
+                    name: "iphone 13",
+                    price: 1650000,
+                    src: "/dist/iphone13.jpeg"
+                }, {
+                    name: "Pressure cooker",
+                    price: 450000,
+                    src: "/dist/pressure cooker.jpeg"
+                }, {
+                    name: "HP Laptop",
+                    price: 4650000,
+                    src: "/dist/hp laptop.jpeg"
+                },
+                {
+                    name: 'Analog watch',
+                    price: 500000,
+                    src: '/dist/watch.jpg'
+                }
+            ]
+            console.log('products are', myproducts)
+            return myproducts;
+            //return this.$store.getters.getProducts
+        }
     }
+}
 
-	/*
-	products: [
-				{
-					name: "Samsung S7 Edged",
-					price: 450000,
-					src: "https://logenson.sirv.com/campushub/s7_2.jpeg"
-				}, {
-					name: "Samsung S8",
-					price: 650000,
-					src: "https://logenson.sirv.com/campushub/s8_1.jpeg"
-				}, 	{
-					name: "Samsung S7 Edged",
-					price: 450000,
-					src: "https://logenson.sirv.com/campushub/s7_2.jpeg"
-				}, {
-					name: "Samsung S8",
-					price: 650000,
-					src: "https://logenson.sirv.com/campushub/s8_1.jpeg"
-				}, 
-			]
-			*/
+
 </script>
 
 <style scoped>
 #shop__products {
     display: flex;
     flex-direction: row;
-	flex-wrap: wrap;
+    flex-wrap: wrap;
     align-items: center;
-	justify-content: center;
+    justify-content: center;
 }
 </style>
